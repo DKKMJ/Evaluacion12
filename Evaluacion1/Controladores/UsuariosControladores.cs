dc03371c8de906be6e8952d31d4fc1badcd694fc
jesus atencio <DeahtDarkPlayer@gmail.com>
2023-09-01 11:43:30 -0400

    SE AGREGARON NUEVOS BOTONES Y  SE PUSIERON NUEVAS FUNCIONALIDADES Y CORRECCION DE ERRORRES

--- Evaluacion1/Controladores/UsuariosControladores.cs
+++ Evaluacion1/Controladores/UsuariosControladores.cs
@@ -2,13 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Evaluacion1.Controladores
 {
     public class UsuariosControladores : Controlador
     {
+        private List<Usuario> dgUsuarios = new List<Usuario>(); // Declarar e inicializar la lista
 
         public List<Usuario> ObtenerListaUsuarios()
         {
@@ -24,20 +23,44 @@ namespace Evaluacion1.Controladores
         {
             datos.Usuarios.RemoveAt(index);
         }
-
-        public void ActualizarUsuario(int index, Usuario usuario)
+        public Usuario ObtenerUsuarioPorRut(string rut)
         {
-            Usuario usuarioExistente = datos.Usuarios[index];
+            return datos.Usuarios.FirstOrDefault(usuario => usuario.Rut == rut);
+        }
 
-            usuarioExistente.Rut = usuario.Rut;
-            usuarioExistente.Nombre = usuario.Nombre;
-            usuarioExistente.Empresa = usuario.Empresa;
-            usuarioExistente.Telefono = usuario.Telefono;
-            usuarioExistente.Direccion = usuario.Direccion;
-            usuarioExistente.FechaRegistro = usuario.FechaRegistro;
-            usuarioExistente.CantidadFacturas = usuario.CantidadFacturas;
-            usuarioExistente.NumeroUltimaFactura = usuario.NumeroUltimaFactura;
-            usuarioExistente.MontoUltimaFactura = usuario.MontoUltimaFactura;
+        public void GuardarUsuarioSinValidacion(Usuario usuario)
+        {
+            // Simplemente guardar el usuario sin realizar validaciones adicionales
+            // Asegúrate de agregar el usuario a tu lista o base de datos aquí
+            dgUsuarios.Add(usuario); // Agregar el usuario a la lista
+        }
+
+        public bool ExisteUsuarioConRut(string rut)
+        {
+            // Verificar si existe un usuario con el mismo Rut
+            return datos.Usuarios.Any(usuario => usuario.Rut == rut);
+        }
+        public bool ExisteUsuarioConNumeroUltimaFactura(int numeroUltimaFactura)
+        {
+            // Verificar si existe un usuario con el mismo número de última factura
+            return datos.Usuarios.Any(usuario => usuario.NumeroUltimaFactura == numeroUltimaFactura);
+        }
+        public void ActualizarUsuario(Usuario usuario)
+        {
+            // Encuentra el usuario en tu lista y actualiza sus datos
+            Usuario usuarioExistente = datos.Usuarios.FirstOrDefault(u => u.Rut == usuario.Rut);
+
+            if (usuarioExistente != null)
+            {
+                usuarioExistente.Nombre = usuario.Nombre;
+                usuarioExistente.Telefono = usuario.Telefono;
+                usuarioExistente.Direccion = usuario.Direccion;
+                usuarioExistente.FechaRegistro = usuario.FechaRegistro;
+                usuarioExistente.Empresa = usuario.Empresa;
+                usuarioExistente.CantidadFacturas = usuario.CantidadFacturas;
+                usuarioExistente.NumeroUltimaFactura = usuario.NumeroUltimaFactura;
+                usuarioExistente.MontoUltimaFactura = usuario.MontoUltimaFactura;
+            }
         }
 
     }
---
+++ Evaluacion1/UF/Api.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
+namespace Evaluacion1.UF
+{
+    public class Api
+    {
+        private static readonly string svcURL = "https://www.mindicador.cl";
+        private static readonly string pathAPI = "api";
+        private static readonly string fecha = DateTime.Now.ToString("dd-MM-yyyy");
+
+        public static string LlamaServicio(string moneda)
+        {
+            string EmpResponse = string.Empty;
+            var qryString = string.Concat(pathAPI, "/", moneda, "/", fecha);
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(svcURL);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage Res = client.GetAsync(qryString).Result;
+                if (Res.IsSuccessStatusCode)
+                {
+                    EmpResponse = Res.Content.ReadAsStringAsync().Result;
+                }
+            }
+            return EmpResponse;
+        }
+
+        public static string ObtenerUf()
+        {
+            string response = string.Empty;
+            var result = LlamaServicio("uf");
+            JObject json = JObject.Parse(result);
+            response = json["serie"][0]["valor"].ToString();
+            return response;
+        }
+
+        public static string ObtenerDolar()
+        {
+            string response = string.Empty;
+            var result = LlamaServicio("dolar");
+            JObject json = JObject.Parse(result);
+            response = json["serie"][0]["valor"].ToString();
+            return response;
+        }
+    }
+}
